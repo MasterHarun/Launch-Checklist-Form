452a71472aca07d891e2a6ff6b29e301b52d9ea6
Harun <master.harun@gmail.com>
2020-05-12 23:34:16 -0400

    forgot to save changes, updated version

--- script.js
+++ script.js
@@ -12,52 +12,51 @@ let fuelStatus = document.getElementById("fuelStatus");
 let cargoStatus = document.getElementById("cargoStatus");
 let button = document.getElementById("formSubmit");
 
-// alert box when submit event triggered
-window.addEventListener("load", function() {
-   form.addEventListener("submit", function(event) {
-      alert("submit clicked");
-   })
-})
 
-//needs all input values to be filled
 window.addEventListener("load", function() {
-         form.addEventListener("submit", function(event) {
+      this.fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+         response.json().then(function(json) {
+            const div = document.getElementById("missionTarget");
+            div.innerHTML =
+               `<h2>Mission Destination</h2>
+               <ol>
+                  <li>Name: ${json.name}</li>
+                  <li>Diameter: ${json.diameter}</li>
+                  <li>Star: ${json.star}</li>
+                  <li>Distance from Earth: ${json.distance}</li>
+                  <li>Number of Moons: ${json.moons}</li>
+               </ol>
+               <img src="${}">`
+         });
+      });
+      form.addEventListener("submit", function(event) {
             if (pilot.value === "" || coPilot.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
                alert("All fields are required!");
             } else if (!isNaN(pilot.value) || !isNaN(coPilot.value) || isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
                alert("Please enter a valid entry!")
             }
 
-         })
-         event.preventDefault();
+
+            event.preventDefault();
+         )
       }
-      //
-      let button = document.getElementById("formSubmit"); button.addEventListener("click", function() {
-         let input = document.getElementById("pilotName");
-      }) window.addEventListener("load", function() {
-         this.fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
-            response.json().then(function(json) {
-               const div = document.getElementById("missionTarget");
-               div.innerHTML = `<h2>Mission Destination</h2>
-         <ol>
-            <li>Name: ${json.name}</li>
-            <li>Diameter: ${json.diameter}</li>
-            <li>Star: ${json.star}</li>
-            <li>Distance from Earth: ${json.distance}</li>
-            <li>Number of Moons: ${json.moons}</li>
-         </ol>
-         <img src="${}">`
-            })
-         })
-      })
-      /* This block of code shows how to format the HTML once you fetch some planetary JSON!
-      <h2>Mission Destination</h2>
-      <ol>
-         <li>Name: ${json.name}</li>
-         <li>Diameter: ${json.diameter}</li>
-         <li>Star: ${json.star}</li>
-         <li>Distance from Earth: ${json.distance}</li>
-         <li>Number of Moons: ${json.moons}</li>
-      </ol>
-      <img src="${}">
-      */
+   )
+}
+//
+// let button = document.getElementById("formSubmit"); button.addEventListener("click", function() {
+//    let input = document.getElementById("pilotName");
+// })
+
+
+
+/* This block of code shows how to format the HTML once you fetch some planetary JSON!
+<h2>Mission Destination</h2>
+<ol>
+   <li>Name: ${json.name}</li>
+   <li>Diameter: ${json.diameter}</li>
+   <li>Star: ${json.star}</li>
+   <li>Distance from Earth: ${json.distance}</li>
+   <li>Number of Moons: ${json.moons}</li>
+</ol>
+<img src="${}">
+*/
